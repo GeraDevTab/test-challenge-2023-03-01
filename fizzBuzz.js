ad3040c6d4772a4035b403bc30e1c4f7725146aa
GerardoDev <everythingissofast@gmail.com>
2023-03-02 16:05:04 -0600

    iniciado parte logica de la funcion de comprobacion de divisibilidad de un numero

---
+++ fizzBuzz.js
@@ -0,0 +1,13 @@
+
+let numeroInicio = 1;
+let numeroFin = 100;
+
+function getRandomIntInclusive(numeroInicio, ) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  let numeroCualquiera = getRandomIntInclusive(numeroInicio,numeroFin);
+
+  
