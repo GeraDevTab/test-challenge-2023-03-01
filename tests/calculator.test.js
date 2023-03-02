4b1d4604c9fd22c19355dccbba363d1686858de8
GerardoDev <everythingissofast@gmail.com>
2023-03-02 15:43:52 -0600

    se realizo un test para verificar a los usuarios, usando el id de cada usuario y comprobando 1 de sus datos de manera aleatoria

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,24 +1,25 @@
-const { default: expect } = require('expect');
+//const { default: expect } = require('expect');
 
+const { power } = require('../calculator');
 const calculator = require('../calculator');
 
 test('adds a + b', () => {
-    //
+    expect(calculator.sum(5,8)).toBe(13);
 })
 
 test('substracts a - b', () => {
-   //
+   expect(calculator.substract(10,3)).toBe(7);
 })
 
 test('multiplies a * b', () => {
-    //
+    expect(calculator.multiply(3,3)).toBe(9)
 })
 
 test('divides a / b', () => {
     // Consider decimals
+    expect(calculator.divide(6,2)).toBe(3)
 })
 
 test('Raises to power a ^ b', () => {
-    //
+    expect(calculator.power(6,3)).toBe(216)
 })
-
--- tests/users.test.js
+++ tests/users.test.js
@@ -1,9 +1,25 @@
 // You already have the methods imported
-const { default: expect } = require('expect');
+//const { default: expect } = require('expect');
 const {getUser, getUsers} = require('../users');
 
 
 // Make a test to verify if the method getUser(id) works correctly
 // Verify any data of the resulting object you want
+test('Vericando un dato de usuario',()=>{
+    expect(getUser(1).email).toBe('sirious90@gmail.com');
+});
+
+test('Vericando un dato de usuario',()=>{
+    expect(getUser(2).username).toBe('piderman');
+});
+
+test('Vericando un dato de usuario',()=>{
+    expect(getUser(3).password).toBe('12345abcde');
+});
+
+test('Vericando un dato de usuario',()=>{
+    expect(getUser(4).password).toBe('Kashmir1970');
+});
+
 
 // READ documentation
