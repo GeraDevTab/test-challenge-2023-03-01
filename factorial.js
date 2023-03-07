61b79086968594413f2e3892881673053aa5c61e
GerardoDev <everythingissofast@gmail.com>
2023-03-07 07:46:07 -0600

    archvivo factorial y su respectivo test

---
+++ factorial.js
@@ -0,0 +1,16 @@
+//3. Calculate the factorial of a number using recursion
+
+let factorial = function factorialDeUnNumero(n){
+    if(n==0){
+        return 1;
+    }
+        else {
+            return n* factorialDeUnNumero(n-1);
+        }
+        
+    }
+
+
+   // console.log("El factorial es de : ",factorial(4));
+
+ module.exports = factorial;
---
+++ tests/factorial.test.js
@@ -0,0 +1,6 @@
+let factorial = require('../factorial');
+
+
+test('El fatorial del numero 6 es ', () => {
+    expect(factorial(6)).toBe(720);
+} )
